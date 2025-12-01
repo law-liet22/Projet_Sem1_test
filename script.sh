3d20d4a4407bb5dde6776b0c9cf0a565b17d680f
law-liet22 <matimoce@icloud.com>
2025-12-01 17:32:04 +0100

    Refactor script.sh to create output directories before compilation; update Java compilation command for clarity and consistency.

--- script.sh
+++ script.sh
@@ -1,3 +1,18 @@
+mkdir out 
+mkdir out/C
+mkdir out/app
+mkdir out/models
+
 gcc -o out/C/triDonnees -c src/services/triDonnees.c
 
+javac -d out src/app/Main.java src/models/*.java
+
+sleep 3s
+
 java -cp out app.Main
+
+read -p "Supprimer les fichiers compilés ? (O/N) : " confirm && [[&confirm = [oO] || confirm = [oO][uU][iI]]] || exit 1
+
+if [ $confirm -eq ]
+
+rm -r out
